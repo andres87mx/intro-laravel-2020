6085d6c57806935941f180ed3e4d0ab73b2ae0c9
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-05-05 17:02:38 -0500

    Clase 5

--- ciclo/app/Http/Controllers/UserController.php
+++ ciclo/app/Http/Controllers/UserController.php
@@ -2,16 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
-    { }
+    {
+        $users = User::latest()->get();
+        return view('users.index', [
+            'users' => $users
+        ]);
+    }
+    public function store(Request $request)
+    {
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
 
-    public function store()
-    { }
+        return back();
+    }
+    public function destroy(User $user)
+    {
+        $user->delete();
 
-    public function destroy()
-    { }
+        return back();
+    }
 }
